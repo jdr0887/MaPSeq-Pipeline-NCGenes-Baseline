4b2df7af1693d1dbbf9fc30e1b75aab743758fed
Jason Reilly <jdr0887@gmail.com>
2016-03-30 14:19:11 -0400

    renaming

---
+++ ncgenes-baseline-executor/src/main/java/edu/unc/mapseq/executor/ncgenes/baseline/NCGenesBaselineWorkflowExecutorTask.java
@@ -0,0 +1,82 @@
+package edu.unc.mapseq.executor.ncgenes.baseline;
+
+import java.util.Date;
+import java.util.List;
+import java.util.TimerTask;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.unc.mapseq.dao.MaPSeqDAOException;
+import edu.unc.mapseq.dao.WorkflowDAO;
+import edu.unc.mapseq.dao.WorkflowRunAttemptDAO;
+import edu.unc.mapseq.dao.model.Workflow;
+import edu.unc.mapseq.dao.model.WorkflowRunAttempt;
+import edu.unc.mapseq.workflow.WorkflowBeanService;
+import edu.unc.mapseq.workflow.WorkflowExecutor;
+import edu.unc.mapseq.workflow.WorkflowTPE;
+import edu.unc.mapseq.workflow.ncgenes.baseline.NCGenesBaselineWorkflow;
+
+public class NCGenesBaselineWorkflowExecutorTask extends TimerTask {
+
+    private final Logger logger = LoggerFactory.getLogger(NCGenesBaselineWorkflowExecutorTask.class);
+
+    private final WorkflowTPE threadPoolExecutor = new WorkflowTPE();
+
+    private WorkflowBeanService workflowBeanService;
+
+    public NCGenesBaselineWorkflowExecutorTask() {
+        super();
+    }
+
+    @Override
+    public void run() {
+        logger.info("ENTERING run()");
+
+        threadPoolExecutor.setCorePoolSize(workflowBeanService.getCorePoolSize());
+        threadPoolExecutor.setMaximumPoolSize(workflowBeanService.getMaxPoolSize());
+
+        logger.info(String.format("ActiveCount: %d, TaskCount: %d, CompletedTaskCount: %d", threadPoolExecutor.getActiveCount(),
+                threadPoolExecutor.getTaskCount(), threadPoolExecutor.getCompletedTaskCount()));
+
+        WorkflowDAO workflowDAO = this.workflowBeanService.getMaPSeqDAOBeanService().getWorkflowDAO();
+        WorkflowRunAttemptDAO workflowRunAttemptDAO = this.workflowBeanService.getMaPSeqDAOBeanService().getWorkflowRunAttemptDAO();
+
+        try {
+            List<Workflow> workflowList = workflowDAO.findByName("NCGenesBaseline");
+            if (workflowList == null || (workflowList != null && workflowList.isEmpty())) {
+                logger.error("No Workflow Found: {}", "NCGenesBaseline");
+                return;
+            }
+            Workflow workflow = workflowList.get(0);
+            List<WorkflowRunAttempt> attempts = workflowRunAttemptDAO.findEnqueued(workflow.getId());
+            if (attempts != null && !attempts.isEmpty()) {
+                logger.info("dequeuing {} WorkflowRunAttempt", attempts.size());
+                for (WorkflowRunAttempt attempt : attempts) {
+
+                    NCGenesBaselineWorkflow ncGenesWorkflow = new NCGenesBaselineWorkflow();
+                    attempt.setVersion(ncGenesWorkflow.getVersion());
+                    attempt.setDequeued(new Date());
+                    workflowRunAttemptDAO.save(attempt);
+
+                    ncGenesWorkflow.setWorkflowBeanService(workflowBeanService);
+                    ncGenesWorkflow.setWorkflowRunAttempt(attempt);
+                    threadPoolExecutor.submit(new WorkflowExecutor(ncGenesWorkflow));
+
+                }
+            }
+        } catch (MaPSeqDAOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public WorkflowBeanService getWorkflowBeanService() {
+        return workflowBeanService;
+    }
+
+    public void setWorkflowBeanService(WorkflowBeanService workflowBeanService) {
+        this.workflowBeanService = workflowBeanService;
+    }
+
+}
---
+++ ncgenes-baseline-messaging/src/main/java/edu/unc/mapseq/messaging/ncgenes/baseline/NCGenesBaselineMessageService.java
@@ -0,0 +1,75 @@
+package edu.unc.mapseq.messaging.ncgenes.baseline;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NCGenesBaselineMessageService {
+
+    private final Logger logger = LoggerFactory.getLogger(NCGenesBaselineMessageService.class);
+
+    private Connection connection;
+
+    private Session session;
+
+    private ConnectionFactory connectionFactory;
+
+    private NCGenesBaselineMessageListener messageListener;
+
+    private String destinationName;
+
+    public NCGenesBaselineMessageService() {
+        super();
+    }
+
+    public void start() throws Exception {
+        logger.info("ENTERING start()");
+        this.connection = connectionFactory.createConnection();
+        this.session = this.connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = this.session.createQueue(this.destinationName);
+        MessageConsumer consumer = this.session.createConsumer(destination);
+        consumer.setMessageListener(messageListener);
+        this.connection.start();
+    }
+
+    public void stop() throws Exception {
+        logger.info("ENTERING stop()");
+        if (this.session != null) {
+            this.session.close();
+        }
+        if (this.connection != null) {
+            this.connection.stop();
+            this.connection.close();
+        }
+    }
+
+    public ConnectionFactory getConnectionFactory() {
+        return connectionFactory;
+    }
+
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    public NCGenesBaselineMessageListener getMessageListener() {
+        return messageListener;
+    }
+
+    public void setMessageListener(NCGenesBaselineMessageListener messageListener) {
+        this.messageListener = messageListener;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+}
