178367840186fdffd8ccb44b0b089d6000971e3c
Jason Reilly <jdr0887@gmail.com>
2016-06-28 14:36:06 -0400

    using version from bundle

--- ncgenes-baseline-executor/src/main/java/edu/unc/mapseq/executor/ncgenes/baseline/NCGenesBaselineWorkflowExecutorTask.java
+++ ncgenes-baseline-executor/src/main/java/edu/unc/mapseq/executor/ncgenes/baseline/NCGenesBaselineWorkflowExecutorTask.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.TimerTask;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -60,13 +63,17 @@ public class NCGenesBaselineWorkflowExecutorTask extends TimerTask {
                 return;
             }
 
+            BundleContext bundleContext = FrameworkUtil.getBundle(getClass()).getBundleContext();
+            Bundle bundle = bundleContext.getBundle();
+            String version = bundle.getVersion().toString();
+
             List<WorkflowRunAttempt> attempts = workflowRunAttemptDAO.findEnqueued(workflow.getId());
             if (CollectionUtils.isNotEmpty(attempts)) {
                 logger.info("dequeuing {} WorkflowRunAttempt", attempts.size());
                 for (WorkflowRunAttempt attempt : attempts) {
 
                     NCGenesBaselineWorkflow ncGenesWorkflow = new NCGenesBaselineWorkflow();
-                    attempt.setVersion(ncGenesWorkflow.getVersion());
+                    attempt.setVersion(version);
                     attempt.setDequeued(new Date());
                     workflowRunAttemptDAO.save(attempt);
 
